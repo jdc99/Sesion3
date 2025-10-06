0c8baf051c8cabe5dcbc99e9dd87b069b4119706
ifram <iframeestudios@gmail.com>
2025-10-06 17:27:56 +0200

    1º test - Creacion de test

---
+++ src/test/CuentaTest.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * 
+ */
+class CuentaTest {
+
+	Cuenta cuenta;
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		cuenta = new Cuenta(0);
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testIngresar() {
+		cuenta.ingresar(500);
+		assertEquals(500, cuenta.getSaldo());
+	}
+	
+	@Test
+	void testRetirar() {
+		cuenta.retirar(500);
+		assertEquals(-500, cuenta.getSaldo());
+	}
+
+}
